2132b029b1535b389cb8c535814ddd56938338d2
김인로 <Kiminlo@Highwillui-Mac-mini.local>
2019-07-31 17:10:38 +0900

    1

--- photos/tests.py
+++ photos/tests.py
@@ -1,19 +1,28 @@
 from django.test import TestCase
 from elasticsearch import Elasticsearch
 import json
+import textract
 
 class ElasticSearchTest(TestCase):
 
     def test_index(self):
+        print("text")
+        text = textract.process("/Users/Kiminlo/Downloads/111.doc")
+        text2 = text.decode('utf-8')
+        print(text2)
+        # es=Elasticsearch([{'host':'localhost','port':9200}])
+        #
+        # res= es.search(index='megacorp',body={
+        #         'query':{
+        #             'match':{
+        #                 "about":"play cricket"
+        #             }
+        #         }
+        #     })
+        # print(json.dumps(res,indent=2))
+        # pass
 
-        es=Elasticsearch([{'host':'localhost','port':9200}])
-
-        res= es.search(index='megacorp',body={
-                'query':{
-                    'match':{
-                        "about":"play cricket"
-                    }
-                }
-            })
-
-        print(json.dumps(res,indent=2))
+    # def texttract1(self):
+    #     print("text")
+    #     text = textract.process("/Users/Kiminlo/Downloads/111.doc ")
+    #     print(text)
