fd10cb5b533b05fbbee0f53ed39b14cf9630f9ac
김인로 <Kiminlo@Highwillui-Mac-mini.local>
2019-08-05 16:24:58 +0900

    1

--- photos/search.py
+++ photos/search.py
@@ -1,11 +1,39 @@
 from elasticsearch import Elasticsearch
 import textract
+import json
 
 class Util():
+
+    def connection(self):
+        es=Elasticsearch([{'host':'localhost','port':9200}])
+        return es
 
     def bin2txt(self, file_name):
         text = textract.process(file_name)
         return text.decode('utf-8')
 
-    def search_index():
-        
+    def doc_index(self, title, content):
+        e1={
+            "title":title,
+            "content":content
+        }
+        es=self.connection()
+        res = es.index(index='test1',doc_type='doc',body=e1)
+
+        print(res)
+
+    def search(self, keyword):
+        es=self.connection()
+        res= es.search(index='test1',body={
+                'query':{
+                    'match':{
+                        "content":keyword
+                    }
+                }
+            })
+
+
+        for hit in res['hits']['hits']:
+            print(hit['_source']['content'])
+            print(hit['_score'])
+            print('**********************')
